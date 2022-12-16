f339ac8d722ecc6b5289fa77379202981227a91b
PedroOrtRec <pedro.ortega.dw@immune.institute>
2022-12-16 02:25:31 +0100

    funcionalidades de filtros realizadas. he empezado a crear una especie de pops de alertas para cuando se crea una tarea, aunque no sé si me dará tiempo.

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const defaultArray = new Array({
 });
 
 let sectionTasks = document.querySelector('.tasks');
+let sectionAlerts = document.querySelector('.alerts')
 let addButton = document.querySelector('#add_button');
 let textAdd = document.querySelector('#text_add');
 let priorityAdd = document.querySelector('#priority_add');
@@ -24,6 +25,28 @@ let spanUserName = document.querySelector('#user');
 
 
 addButton.addEventListener('click', getValues);
+
+textSearch.addEventListener('keydown', filterByName);
+
+prioritySearch.addEventListener('change', filterByPriority)
+
+function filterByName(event) {
+    if (event.keyCode === 13) {
+        let filterArray = pullArrays();
+        filterArray = filterArray.filter(task => task.title.toLowerCase().includes(textSearch.value.toLowerCase()));
+        printTasks(filterArray, sectionTasks);
+    }
+}
+
+function filterByPriority(event) {
+    if (event.target.value != 'all') {
+        let filterArray = pullArrays();
+        filterArray = filterArray.filter(task => task.priority === event.target.value);
+        printTasks(filterArray, sectionTasks);
+    } else {
+        printTasks(pullArrays(), sectionTasks);
+    }
+}
 
 function startUser() {
     let verificationUser = localStorage.getItem('userName16');
@@ -76,6 +99,7 @@ function printOneTask(pTask, pIdTask, pDom) {
             article.className += ' monthly_task';
             break;
     }
+
     let h3 = document.createElement('h3');
     h3.innerText = pTask.title;
     let button = document.createElement('button');
@@ -100,13 +124,12 @@ function getValues() {
     let priority = priorityAdd.value;
     if (priority !== 'none') {
         addTask(title, priority);
+        textAdd.value = '';
+        priorityAdd.value = 'none';
     }
     else {
         alert('Elige una prioridad para la tarea')
     };
-    textAdd.value = '';
-    priorityAdd.value = 'none';
-
 };
 
 function addTask(pTitle, pPriority) {
@@ -119,8 +142,22 @@ function addTask(pTitle, pPriority) {
     });
     pushArrays(addArray);
     printTasks(pullArrays(), sectionTasks);
+    printAlerts(sectionAlerts);
 }
 
+function printAlerts(pDom) {
+    let article = document.createElement('article');
+    let h4 = document.createElement('h4');
+    h4.className += 'alert_text'
+    h4.innerText = 'Se ha añadido una nueva tarea'
+    article.className += 'alert_pop'
+    pDom.appendChild(article);
+    article.appendChild(h4);
+}
+
+// function smallerTask(event) {
+//     event.target.parentNode.className += ' smaller_task';
+// }
 
 startTasks();
 startUser();
